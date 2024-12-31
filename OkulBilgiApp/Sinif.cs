cf516f547aa98d4029e493237ed335c098627a82
dlrvzz <148449352+dlrvzz@users.noreply.github.com>
2024-12-31 11:18:13 +0300

    Update Sinif.cs

--- OkulBilgiApp/Sinif.cs
+++ OkulBilgiApp/Sinif.cs
@@ -8,11 +8,12 @@ namespace OkulBilgiApp
 {
     internal class Sinif
     {
-        public int SinifId { get; set; }
-        public string SinifAd { get; set; }
-        public string Kontenjan { get; set; }
+        public int SinifId { get; set; } //primary key
+        public string SinifAd { get; set; } 
+        public string Kontenjan { get; set; } //dersin alabileceği öğrenci sayısı
 
-        public ICollection<Ogrenci>? Ogrenciler { get; set; }
+        public ICollection<Ogrenci>? Ogrenciler { get; set; } //sınıfın öğrencilerle ilişkisi
+                                                             // sınıfa kayıtlı olan öğrencilerin koleksiyonunu tutar.
 
     }
 }
